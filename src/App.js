45a4ede251356fa0afee6782b19f1c61dac529df
ravianumukonda <tejapr221@gmail.com>
2023-01-15 08:40:33 +0000

    usersList

---
+++ src/App.js
@@ -0,0 +1,83 @@
+import {Component} from 'react'
+import UserProfile from './components/UserProfile'
+
+import './App.css'
+
+const initialUserDetailsList = [
+  {
+    uniqueNo: 1,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'Esther Howard',
+    role: 'Software Developer',
+  },
+  {
+    uniqueNo: 2,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/floyd-miles-img.png',
+    name: 'Floyd Miles',
+    role: 'Software Developer',
+  },
+  {
+    uniqueNo: 3,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/jacob-jones-img.png',
+    name: 'Jacob Jones',
+    role: 'Software Developer',
+  },
+  {
+    uniqueNo: 4,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/devon-lane-img.png',
+    name: 'Devon Lane',
+    role: 'Software Developer',
+  },
+]
+
+class App extends Component {
+  state = {
+    searchInput: '',
+    userDetails: initialUserDetailsList,
+  }
+
+  onChangeSearchInput = event => {
+    this.setState({
+      searchInput: event.target.value,
+    })
+  }
+
+  onChangeDelete = uniqueNo => {
+    const {userDetails} = this.state
+
+    const filteredUser = userDetails.filter(
+      eachUser => eachUser.uniqueNo !== uniqueNo,
+    )
+    this.setState({userDetails: filteredUser})
+  }
+
+  render() {
+    const {searchInput, userDetails} = this.state
+
+    const searchResult = userDetails.filter(eachResult =>
+      eachResult.name.toLowerCase().includes(searchInput),
+    )
+
+    return (
+      <div className="app-container">
+        <h1 className="title">Users List</h1>
+        <input
+          type="search"
+          onChange={this.onChangeSearchInput}
+          value={searchInput}
+        />
+        <ul className="list-container">
+          {searchResult.map(eachUser => (
+            <UserProfile
+              onChangeDelete={this.onChangeDelete}
+              userDetails={eachUser}
+              key={eachUser.uniqueNo}
+            />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default App
